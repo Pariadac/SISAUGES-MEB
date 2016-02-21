3de2af227ab367f8f36c5d50f048e6698fe46462
Pariadac <zero_zerouno@hotmail.com>
2016-02-21 11:25:52 -0430

    vistaprincipal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Muestrario</title>
+    <title>Muestrario2</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="{{url('assets/css/bootstrap.min.css') }}" rel="stylesheet">
