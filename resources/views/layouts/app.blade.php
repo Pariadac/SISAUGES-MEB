8adb5907b76bcb9773acdad9ee2f517dcae0b5f4
Ely J. Colmenarez M <ElyJColmenarez@gmail.com>
2016-03-05 17:33:43 -0430

    inhabilitado el boton de regresar del navegador, como parte de la seguridad del sistema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,17 +33,17 @@
     <link href="{{url('assets/css/principal-style.css') }}" rel="stylesheet">
 
     <link href="{{url('assets/css/jquery.datetimepicker.css') }}" rel="stylesheet">
-    <script type="text/javascript">
-        window.history.forward();
-        function noBack()
-        {
-            window.history.forward();
-        }
+    <script type = "text/javascript" >
+        function nobackbutton(){
+           window.location.hash="no-back-button";
+           window.location.hash="Again-No-back-button" //chrome
+           window.onhashchange=function(){window.location.hash="no-back-button";}
+          }
     </script>
 
 </head>
 
-<body  onLoad="noBack();" onpageshow="if (event.persisted) noBack();" onUnload="">
+<body  onLoad="nobackbutton();" onpageshow="if (event.persisted) nobackbutton();" onUnload="">
 
     @if (Auth::guest())<div id="wrapper" >@else <div id="wrapper"> @endif
 
